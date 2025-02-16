f71b21964e06a728e2adfbd8df0c4aa28d1677a2
Björn Stresing <bjoern.stresing@gmx.de>
2025-02-16 16:30:31 +0100

    Fix mingw build

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,12 @@
-from conan import ConanFile, conan_version
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json, os
+from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.apple import is_apple_os
-from conan.tools.build import cross_building
-from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
-from conan.tools.files import (
-    apply_conandata_patches, chdir, copy, export_conandata_patches, get, rename,
-    replace_in_file, rm, rmdir, save, load
-)
-from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, PkgConfigDeps
-from conan.tools.layout import basic_layout
-from conan.tools.microsoft import check_min_vs, is_msvc, unix_path
+from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.files import  patch, copy, get
 from conan.tools.scm import Git
-import os
-import glob
-import shutil
-import re
-import json
 
 required_conan_version = ">=2.0"
 
@@ -36,9 +27,9 @@ class FFMpegConan(ConanFile):
     url = jsonInfo["repository"]
     # ---Sources---
     exports = ["info.json"]
-    exports_sources = []
+    exports_sources = ["patches/*"]
     # ---Requirements---
-    tool_requires = ["ninja/[>=1.11.1]"]
+    tool_requires = []
     # ---Binary model---
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
@@ -111,17 +102,24 @@ class FFMpegConan(ConanFile):
     no_copy_source = True
 
     @property
+    def is_mingw(self):
+        return self.settings.os == "Windows" and self.settings.compiler == "gcc"
+
+    @property
     def avbuild_arch(self):
         arch = {
             "x86": "x86",
             "x86_64": "x86_64" if self.settings.os == 'Android' else 'amd64',
             "armv8": "arm64" if self.settings.os == 'Macos' else 'armv8',
             "armv7": "armv7",
+            "armv6": "armv6",
         }
         return arch.get(str(self.settings.arch))
     
     @property
     def avbuild_os(self):
+        if self.is_mingw:
+            return "mingw"
         return str(self.settings.os).lower()
     
     @property
@@ -137,6 +135,9 @@ class FFMpegConan(ConanFile):
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
+    def adjust_path(self, path):
+        return path.replace("\\", "/")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination="ffmpeg", strip_root=True)
     
@@ -149,6 +150,18 @@ class FFMpegConan(ConanFile):
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                 self.tool_requires("msys2/cci.latest")
 
+    def validate(self):
+        valid_os = ["Windows", "Linux", "Android", "Macos"]
+        if str(self.settings.os) not in valid_os:
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} is only supported for the following operating systems: {valid_os}")
+        valid_arch = ["x86_64", "x86", "armv6", "armv7", "armv8"]
+        if str(self.settings.arch) not in valid_arch:
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} is only supported for the following architectures on {self.settings.os}: {valid_arch}")
+        if self.settings.os == 'Windows' and not self.settings.compiler == "gcc":
+            raise ConanInvalidConfiguration("On windows only a mingw build is supported")
+
     def generate(self):
         ms = VirtualBuildEnv(self)
         ms.generate()
@@ -158,23 +171,28 @@ class FFMpegConan(ConanFile):
         git.clone(url="https://github.com/wang-bin/avbuild.git", args=["--recurse-submodules", "--shallow-submodules"], target="avbuild")
         git.folder = "avbuild"
         git.checkout("80373c7a7eecabfb479f6ff06fc4b3e0f22d6c79")
+        patch(self, base_path=os.path.join(self.build_folder, "avbuild"), patch_file=os.path.join(self.source_folder, "patches", "missing_symbol_to_upper4.patch"))
 
         f = open(os.path.join("avbuild", "config.sh"), "w")
         f.close()
-        #shutil.copyfile(os.path.join("avbuild", "config-lite.sh"), os.path.join("avbuild", "config.sh"))
 
         env1 = Environment()
         env1.define("USE_TOOLCHAIN", self.avbuild_compiler)
-        env1.define("FFSRC", self.source_folder + "/ffmpeg")
+        env1.define("FFSRC", self.adjust_path(os.path.join(self.source_folder, "ffmpeg")))
 
-        options = ['--disable-autodetect', '--disable-programs', '--disable-doc', '--disable-libdrm', '--disable-everything']
+        options = ['--disable-autodetect', '--disable-programs', '--disable-doc', '--disable-libdrm', '--disable-everything', '--disable-os2threads', '--enable-gpl', '--enable-version3']
+        if self.is_mingw:
+            if getattr(getattr(self.settings, 'compiler'), 'threads') == 'posix':
+                options.append('--disable-w32threads')
+            if getattr(getattr(self.settings, 'compiler'), 'threads') == 'win32':
+                options.append('--disable-pthreads')
 
         env1.define("USER_OPT", " ".join(options))
 
         #env1.define("DEC_OPT_MOBILE", "--enable-decoder=*sub*,movtext,*web*,aac*,ac3*,eac3*,alac*,ape,ass,av1*,ccaption,cfhd,cook,dca,dnxhd,exr,truehd,*yuv*,flv,flac,gif,h26[3-4]*,hevc*,hap,mp[1-3]*,prores,*[mj]peg*,mlp,mpl2,nellymoser,opus,pcm*,qtrle,*png*,tiff,rawvideo,rv*,sami,srt,ssa,v210*,vc1*,vorbis,vp[6-9]*,wm*,wrapped_avframe")
 
         with env1.vars(self).apply():
-            self.run("%s %s %s" % (self.build_folder + "/avbuild/avbuild.sh", self.avbuild_os, self.avbuild_arch), cwd=os.path.join(self.build_folder, "avbuild"))
+            self.run("%s %s %s" % (self.adjust_path(os.path.join(self.build_folder, "avbuild", "avbuild.sh")), self.avbuild_os, self.avbuild_arch), cwd=self.adjust_path(os.path.join(self.build_folder, "avbuild")))
 
     def package(self):
         out_dir = "sdk-%s-%s-%s" % (self.avbuild_os, self.avbuild_arch, self.avbuild_compiler)
